9367e89fe22eb33c0b81bea35ac54648f59e607b
Miguel <miguel.espinoza.silloca@gmail.com>
2024-12-27 19:57:07 -0700

    - adding prettier - deleting comments - improving text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://10iguel.github.io',
-  base: 'portfolio',
-})
+    integrations: [tailwind(), robotsTxt()],
+    site: 'https://10iguel.github.io',
+    base: 'portfolio',
+});
